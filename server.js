b2343d26ea72bbabc663fccb23e64215e7949035
Linh <linhdr141020@gmail.com>
2024-12-08 17:43:42 +0700

    a

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const fs = require("fs") 
+const fs = require("fs")
 const https = require("https")
 const express = require("express")
 const app = express()
 const port = 3000
 const cookieParser = require("cookie-parser")
+const crypto = require("crypto")
 var cors = require('cors')
-
+const { base64url } = require("./helpers")
+const jwtSecret = 'fA/W/G6UlsqQHRbYXBXuXrk2JhL8NHXFX6U2jLm/aun4knzZCfWLWGkov2zOCfVLmXdlO+iB40H1m0WSBXB1wWcumVeL3Bhsk0khrIHvKFcDRLCvvDREGoOS7vkv036Sd+eghBy9kmsKgHR6N9NX3wwA9cFPuHuKQ9K/UBE9SuHofVkjiXI6J8jKBK4JL88Q/fJK/NPjwev+kVOQNIcvJBbZnnXM61upFfl3zcWhnoeaYkDJt10o8n+r2vBkC2ndBlfv1v9p643VgOurTlnEToxMy2ctym/LAIUWR2F7dgFWPfTamnF6/3eUrOuCRS5CXk8iYpwipX1ulVl2HQTnsw==';
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
@@ -91,30 +93,59 @@ app.post("/api/auth/login", (req, res) => {
             })
     }
 
-    const sessionId = Date.now().toString()
-    sessions[sessionId] = { sub: user.id }
+    const header = {
+        alg: "HS256",
+        typ: "JWT"
+    }
+    const payload = {
+        sub: user.id,
+        exp: Date.now() + 3600000
+    }
+    const encodedHeader = base64url(JSON.stringify(header))
+    const encodePayload = base64url(JSON.stringify(payload))
 
+    const tokenData = `${encodedHeader}.${encodePayload}`
+    const hmac = crypto.createHmac("sha256", jwtSecret)
+    const signature = hmac.update(tokenData).digest("base64url")
 
-    res.setHeader("Set-Cookie", `sessionId=${sessionId};httpOnly; max-age=3600`).json(user)
+    res.json({
+        token: `${tokenData}.${signature}`
+    })
 })
 //[GET] /api/auth/me
 app.get("/api/auth/me", (req, res) => {
-    const session = sessions[req.cookies.sessionId]
-    if (!session) {
+    const token = req.headers.authorization?.slice(7)
+    if (!token) {
         return res.status(401)
             .json({
                 message: "Unauthorized"
             })
     }
-    const user = db.users.find(user => user.id == session.sub)
-
-    if (!session) {
+    const [encodedHeader, encodePayload, tokenSignature] = token.split(".")
+    const tokenData = `${encodedHeader}.${encodePayload}`
+    const hmac = crypto.createHmac("sha256", jwtSecret)
+    const signature = hmac.update(tokenData).digest("base64url")
+    if (signature != tokenSignature) {
         return res.status(401)
             .json({
                 message: "Unauthorized"
             })
     }
-    res.json(user)
+    const payload = JSON.parse(atob(encodePayload))
+    const user = db.users.find(user => user.id == payload.sub)
+    if (!user) {
+        return res.status(401)
+            .json({
+                message: "Unauthorized"
+            })
+    }
+    if(payload.exp < Date.now()){
+        return res.status(401)
+        .json({
+            message: "Unauthorized"
+        })
+    }
+    return res.json({ user })
 })
 
 //Session
